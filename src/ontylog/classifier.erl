9e3d10fb5d9b444f7156191c7ec396d0f68f2114
Robert Dionne <bitdiddle@shadow.local>
2010-06-02 07:29:19 -0400

    initial build and topological sort of def_cons

--- src/ontylog/classifier.erl
+++ src/ontylog/classifier.erl
@@ -26,20 +26,13 @@
 
 -include("bitstore.hrl").
 
--export([classify/1,
+-export([classify/2,
          subsumes_p/2]).
 %%
 -import(bitcask, [get/2,put/3,fold/3]).
+-import(digraph, [new/1,add_vertex/1,add_vertex/3,add_edge/4]).
+-import(lists, [map/2]).
 %%
-%%
-classify(Dag) ->
-    %% create new digraph and add vertex for each node in the cask, using the key for the label
-    %% add single edge for each link
-    NewFacts = fold(Dag,
-                    fun(_Id,Concept,Acc) ->
-                            classify(Dag,Concept) ++ Acc
-                    end,[]),
-    store_new_facts(NewFacts).
 %%
 %% 0. topologically sort concepts with respect to the "isa" relation
 %% 1. compute the LUBs for the concept
@@ -58,7 +51,58 @@ classify(Dag) ->
 %% They can be used to store colors, prim/def bits, visited, and classified bits for recursive 
 %% calls
 %%
-classify(_Dag, _Concept) ->    
+%%
+%%
+%% classification in traditional DLs is always with respect to the "isa" relation but
+%% one can imagine other interencing algorithms over different relations
+%%
+classify(DagCask,Arrow) ->
+
+    %% create new digraph and add vertex for each node in the cask, using the key for the label
+    %% add single edge for each link
+    Dag = new([acyclic, private]),
+
+    ets:new(vertex_ids,[set]),
+
+    %% nothing can happen without ***Thing***
+    Root = add_labeled_vertex(Dag,<<"0">>),    
+    
+    %% first get all vertices created so that forward refences can be resolved 
+    fold(DagCask,
+         fun(K,_Concept,_Acc) ->
+                 ets:insert(vertex_ids,{K, add_labeled_vertex(Dag,K)})
+         end,[]),
+
+    %% now add all the edges. Note that we could get all the relations from each node
+    %% in this single pass, which is more efficient, but correctness comes first so 
+    %% we'll keep it dirt simple
+    fold(DagCask,
+         fun(K,Concept,_Acc) ->
+                 case proplists:lookup(Arrow,element(1, Concept)) of
+                     none ->
+                         %% node is a root wrt Arrow relation, add ***Thing*** as parent
+                         add_edge(Dag,find_vertex(K),Root,Arrow);
+                     {Arrow, Targets} ->
+                         map(fun(Target) ->
+                                     add_edge(Dag,find_vertex(K),find_vertex(Target),Arrow)
+                             end,Targets)
+                 end
+         end,[]),
+
+    %% Visit concepts in topological order and classify each
+    Inferences = map(fun(Concept) ->
+                             classify_con(Dag, Concept)
+                     end, 
+                     digraph_utils:topsort(Dag)),
+    %%
+    %% store the new inferecences in the cask
+    map(fun(NewFacts) ->
+                store_new_facts(DagCask,NewFacts)
+        end,Inferences),
+    
+    ok.
+
+classify_con(_Dag, _Concept) ->    
     ok.
                  
 %%
@@ -67,5 +111,18 @@ subsumes_p(_Node1,_Node2) ->
     true.
 %%
 %%
-store_new_facts(_NewFacts) ->
+store_new_facts(_DagCask, _NewFacts) ->
     ok.
+%%
+%%
+add_labeled_vertex(Dag,Label) ->
+    V = add_vertex(Dag),
+    add_vertex(Dag,V,Label).
+%%
+%%
+find_vertex(Key) ->
+    case ets:lookup(vertex_ids,Key) of
+        [] ->
+             [];
+        [{key, V}] -> V
+    end.
