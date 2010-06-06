1e65d95edeaf0239abfe95b223f64ed0f7807776
Robert Dionne <bitdiddle@shadow.local>
2010-06-06 14:46:58 -0400

    added find_glbs and core of is_greater

--- src/ontylog/classifier.erl
+++ src/ontylog/classifier.erl
@@ -32,7 +32,7 @@
 -import(digraph, [new/1,add_vertex/1,add_vertex/3,
                   in_neighbours/2, get_path/3,
                   add_edge/4, out_degree/2, vertex/2]).
--import(lists, [map/2, any/2, reverse/1]).
+-import(lists, [map/2, all/2, any/2, reverse/1]).
 %%
 %%
 -ifdef(TEST).
@@ -69,7 +69,8 @@ classify(DagCask,Arrow,ClassifyFun) ->
     Dag = new([acyclic]),
 
     Vids = ets:new(vertex_ids,[set]),    
-    
+    put(<<"cask">>,DagCask),
+    put(<<"id_tab">>,Vids),
     %% first get all vertices created so that forward refences can be resolved 
     fold(DagCask,
          fun(K,_Concept,Tab) ->
@@ -130,6 +131,7 @@ classify(DagCask,Arrow,ClassifyFun) ->
 classify_con(LookUpTab, Dag, Concept, Arrow) ->
     print_concept(Dag,Concept),
     Thing = find_vertex(thing(),LookUpTab),
+    print_concept(Dag,Thing),
     %% ***Thing*** subsumes all concepts so we start there
     case out_degree(Dag,Concept) of
         0 ->
@@ -145,9 +147,9 @@ classify_con(LookUpTab, Dag, Concept, Arrow) ->
     ?LOG(?DEBUG,"The Lubs are ~p ~n",[Lubs]),
     %%
     %% treating each Lub as a root now find the Glbs
-    Glbs = map(fun(Lub) ->
+    Glbs = sl_flatten(map(fun(Lub) ->
                 find_glbs(Dag,Lub,Concept,[])
-        end,Lubs),
+        end,Lubs)),
     ?LOG(?DEBUG,"The Glbs are ~p ~n",[Glbs]),
     %%
     %% 
@@ -157,43 +159,98 @@ classify_con(LookUpTab, Dag, Concept, Arrow) ->
 %%
 find_lubs(Dag,PossSubsumer,Concept,Lubs) ->
     ?LOG(?DEBUG,"calling find_lubs with ~p ~p ~p ~n",[PossSubsumer,Concept,Lubs]),
+    print_concept(Dag,PossSubsumer),
     NewLubs = case PossSubsumer == Concept of
                   true ->
+                      ?LOG(?DEBUG,"ok, these guyes are eq ~p ~p ~n",[PossSubsumer,Concept]),
                       Lubs;
                   _ ->
                       case is_greater(Dag,PossSubsumer,Concept) of
                           true ->
-                              add_if_least(Dag,PossSubsumer,Lubs);
+                              add_lub(Dag,PossSubsumer,Lubs);
                           _ ->
                               Lubs
                       end
               end,
-    Children = in_neighbours(Dag,PossSubsumer),
-    case Children of
-        [] ->
-            NewLubs;
-        _ ->
-            lists:flatten(map(fun(Child) ->
-                        find_lubs(Dag,Child,Concept,NewLubs)
-                end,Children))
+    case NewLubs /= Lubs of
+        true ->
+            Children = in_neighbours(Dag,PossSubsumer),
+            case Children of
+                [] ->
+                    NewLubs;
+                _ ->
+                    sl_flatten(map(fun(Child) ->
+                                find_lubs(Dag,Child,Concept,NewLubs)
+                        end,Children))
+            end;
+        false -> NewLubs
     end.
 %%
 %%
-find_glbs(_Dag,_PossSubsumee,_Concept,_Glbs) ->
-    %% this is the part I hate the most
-    ok.
-    
+find_glbs(Dag,PossSubsumee,Concept,Glbs) ->
+    ?LOG(?DEBUG,"calling find_glbs with ~p ~p ~p ~n",[PossSubsumee,Concept,Glbs]),
+    print_concept(Dag,PossSubsumee),
+    NewGlbs = case PossSubsumee == Concept of
+                  true ->
+                      ?LOG(?DEBUG,"ok, these guyes are eq ~p ~p ~n",[PossSubsumee,Concept]),
+                      Glbs;
+                  _ ->
+                      case is_greater(Dag,Concept,PossSubsumee) of
+                          true ->
+                              add_glb(Dag,PossSubsumee,Glbs);
+                          _ -> 
+                              Glbs
+                      end
+              end,
+    case NewGlbs /= Glbs of
+        true ->
+            Children = in_neighbours(Dag,PossSubsumee),
+            case Children of
+                [] ->
+                    NewGlbs;
+                _ -> 
+                     sl_flatten(map(fun(Child) ->
+                                find_glbs(Dag,Child,Concept,NewGlbs)
+                        end,Children))
+            end;
+        false -> 
+            NewGlbs
+    end.    
 %%
 %%
-add_if_least(Dag,Lub,Lubs) ->
-    case any(fun(Con) -> 
-                     subsumes_p(Dag,Con,Lub)
-             end,Lubs) of
-        true -> Lubs;
-        false ->
-            lists:append(Lubs,[Lub])
-    end.
-        
+add_lub(Dag,Lub,Lubs) ->
+    add_con_if_satisfies(
+      Dag,
+      Lub,
+      Lubs,
+      fun(Elem) ->
+              subsumes_p(Dag,Elem,Lub)
+      end).
+%%
+%%
+add_glb(Dag,Glb,Glbs) ->
+    add_con_if_satisfies(
+      Dag,
+      Glb,
+      Glbs,
+      fun(Elem) ->
+              subsumes_p(Dag,Glb,Elem)
+      end).
+%%
+%%
+add_con_if_satisfies(Dag,Con,Cons,Fun) ->
+    ?LOG(?DEBUG,"checking adding con to list ~p ~p ~n",[Con,Cons]),
+    print_concept(Dag,Con),
+    ConsToRemove = 
+        lists:foldl(fun(Elem, Acc) ->
+                            case Fun(Elem) of
+                                true -> lists:append(Acc, [Elem]); 
+                                _ -> Acc
+                            end
+                    end,[],Cons),
+    NewCons = lists:subtract(Cons,ConsToRemove),
+    ?LOG(?DEBUG,"ok, adding concept ~p ~n",[Con]),            
+    lists:append(NewCons,[Con]).        
 %%
 %%
 create_inferred_facts(_Dag,_Lubs,_Glbs,_Concept) ->
@@ -201,8 +258,21 @@ create_inferred_facts(_Dag,_Lubs,_Glbs,_Concept) ->
 %%
 %% compare the definitions of two concepts
 %% this is where all the logical work in classification is
-is_greater(_Dag,_Subsumer,_Subsumee) ->
-    ok.
+is_greater(Dag,Subsumer,Subsumee) ->
+    SupKey = extract_key(Dag,Subsumer),
+    SubKey = extract_key(Dag,Subsumee),
+    DagCask = get(<<"cask">>),
+    SupDef = dag:get_targets(SupKey,DagCask),
+    SubDef = dag:get_targets(SubKey,DagCask),
+    ?LOG(?DEBUG,"the definition of ~p is ~p ~n",[SupKey,SupDef]), 
+    ?LOG(?DEBUG,"the definition of  ~p is  ~p ~n",[SubKey,SubDef]),
+    %% all works even if SupDef is empty, as it should given all is vacuously true
+    %% it is a thing of beauty to be able to state something so succintly
+    all(fun(SupRel) ->
+                any(fun(SubRel) ->
+                            rel_subsumes_p(Dag,SupRel,SubRel)
+                    end,SubDef)
+        end,SupDef).
     
    
 %%
@@ -213,7 +283,24 @@ subsumes_p(Dag,Subsumer,Subsumee) ->
         false -> 
             false;
         _ ->
+            ?LOG(?DEBUG,"subsumes_p found a path ~p ~p ~n",[Subsumee,Subsumer]),
             true
+    end.
+%%
+%%
+rel_subsumes_p(Dag,SupRel,SubRel) ->
+    ?LOG(?DEBUG,"checking role vals ~p ~p ~n",[SupRel,SubRel]),
+    IdTab = get(<<"id_tab">>),
+    case element(1,SupRel) == element(1,SubRel) of
+        false ->
+            false;
+        true -> 
+            all(fun(Sup) ->
+                        any(fun(Sub) ->
+                                    subsumes_p(Dag,find_vertex(Sup,IdTab),
+                                               find_vertex(Sub,IdTab))
+                            end,element(2,SubRel))
+                end,element(2,SupRel))
     end.
 %%
 %%
@@ -224,6 +311,10 @@ store_new_facts(_DagCask, _NewFacts) ->
 add_labeled_vertex(Dag,Label) ->
     V = add_vertex(Dag),
     add_vertex(Dag,V,Label).
+%%
+%%
+extract_key(Dag,Concept) ->
+    element(1,element(2,vertex(Dag,Concept))).
 %%
 %%
 find_vertex(Key,Tab) ->
@@ -239,7 +330,14 @@ thing() ->
 %%
 %%
 print_concept(Dag, Concept) ->
-    ?LOG(?DEBUG,"classifying ~p ~n",[element(2,vertex(Dag, Concept))]).
+    ?LOG(?DEBUG,"concept ~p ~n",[element(2,vertex(Dag, Concept))]).
+%%
+%%
+sl_flatten([]) -> [];
+sl_flatten([[]]) -> [];
+sl_flatten([H | T]) ->
+    [hd(H) | sl_flatten(T)].
+
 
 %% 
 %% EUnit tests
